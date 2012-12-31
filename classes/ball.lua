31db62494935aa73135f7c7fe25769e78ac31864
Mike Ambrose <mambrose4@gmail.com>
2012-12-31 18:31:39 -0500

    added graphics to bg and ball

--- classes/ball.lua
+++ classes/ball.lua
@@ -1,12 +1,14 @@
 require "lib/class"
 ball = class:new()
 
-function ball:init(world, x, y)
+function ball:init(world, x, y, image)
 	self.body = love.physics.newBody(world, x, y, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
 	self.shape = love.physics.newCircleShape(4) --the ball is 40 cm
 	self.fixture = love.physics.newFixture(self.body, self.shape, 1) -- Attach fixture to body and give it a density of 1.
 	self.fixture:setRestitution(0.9) --let the ball bounce
 	self.body:setLinearDamping(2);
+	
+	if(image) then self.image=image end
 end
 
 function ball:update(dt)
@@ -15,5 +17,5 @@ end
 
 function ball:kick(angle, magnitude)
 	if angle == 0 then angle=360 end
-	self.body:applyLinearImpulse(math.sin(math.pi*(angle/180)), -math.cos(math.pi*(angle/180))) --0 degrees is up, 90 right, etc
+	self.body:applyForce(math.sin(math.pi*(angle/180))*magnitude*10, -math.cos(math.pi*(angle/180))*magnitude*10) --0 degrees is up, 90 right, etc
 end
--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 require 'classes/ball'
 
 walls=true
+bg=love.graphics.newImage("field.png");
+ballImage=love.graphics.newImage("ball.png");
 
 function love.load()
   love.physics.setMeter(10) --the height of a meter our worlds will be 64px
@@ -18,28 +20,28 @@ function love.load()
 		objects.walls.top.fixture = love.physics.newFixture(objects.walls.top.body, objects.walls.top.shape)
 
 		objects.walls.right = {}
-		objects.walls.right.body = love.physics.newBody(world, 1000, 300)
-		objects.walls.right.shape = love.physics.newRectangleShape(1, 600)
+		objects.walls.right.body = love.physics.newBody(world, 1000, 280)
+		objects.walls.right.shape = love.physics.newRectangleShape(1, 560)
 		objects.walls.right.fixture = love.physics.newFixture(objects.walls.right.body, objects.walls.right.shape)
 
 		objects.walls.bottom = {}
-		objects.walls.bottom.body = love.physics.newBody(world, 500, 600)
+		objects.walls.bottom.body = love.physics.newBody(world, 500, 560)
 		objects.walls.bottom.shape = love.physics.newRectangleShape(1000, 1)
 		objects.walls.bottom.fixture = love.physics.newFixture(objects.walls.bottom.body, objects.walls.bottom.shape)
 
 		objects.walls.left = {}
-		objects.walls.left.body = love.physics.newBody(world, 0, 300)
-		objects.walls.left.shape = love.physics.newRectangleShape(1, 600)
+		objects.walls.left.body = love.physics.newBody(world, 0, 280)
+		objects.walls.left.shape = love.physics.newRectangleShape(1, 560)
 		objects.walls.left.fixture = love.physics.newFixture(objects.walls.left.body, objects.walls.left.shape)
 
 	end
   
   --let's create a ball
-  objects.ball = ball:new(world, 500, 300)
+  objects.ball = ball:new(world, 500, 280, ballImage)
 
   --initial graphics setup
-  love.graphics.setBackgroundColor(72, 160, 14) --set the background color to a nice blue
-  love.graphics.setMode(1000, 600, false, true, 0) --set the window dimensions to 650 by 650
+  --love.graphics.setBackgroundColor(72, 160, 14) --set the background color to a nice blue
+  love.graphics.setMode(1000, 560, false, true, 0) --set the window dimensions to 650 by 650
 end
 
 
@@ -49,17 +51,24 @@ function love.update(dt)
   
 	--here we are going to create some keyboard events
 	if love.keyboard.isDown("right") then
-		objects.ball:kick(90, 1)
+		objects.ball:kick(90, 75)
 	elseif love.keyboard.isDown("left") then
-		objects.ball:kick(270, 1)
+		objects.ball:kick(270, 75)
 	elseif love.keyboard.isDown("down") then
-		objects.ball:kick(180, 1)
+		objects.ball:kick(180, 75)
 	elseif love.keyboard.isDown("up") then
-		objects.ball:kick(0, 1)
+		objects.ball:kick(0, 75)
 	end
 end
 
 function love.draw()
-  love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
-  love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
+	love.graphics.setColor(255,255,255,128)
+	love.graphics.draw(bg)
+	love.graphics.setColor(255,255,255,255)
+	if(objects.ball.image) then love.graphics.draw(objects.ball.image, objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.body:getAngle(),  1, 1, objects.ball.image:getWidth()/2, objects.ball.image:getHeight()/2)
+	else
+		love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
+		ove.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
+	end
+  
 end
