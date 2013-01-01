a7400c1d1f5b445d1132c1995af34fe25dd08aa7
Coda Brink <codabrink@gmail.com>
2012-12-31 19:13:21 -0500

    my changes

--- classes/ball.lua
+++ classes/ball.lua
@@ -2,11 +2,11 @@ require "lib/class"
 ball = class:new()
 
 function ball:init(world, x, y, image)
-   self.body = love.physics.newBody(world, x, y, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
+   self.body = love.physics.newBody(world, x, y, "dynamic")
    self.shape = love.physics.newCircleShape(4) --the ball is 40 cm
-   self.fixture = love.physics.newFixture(self.body, self.shape, 1) -- Attach fixture to body and give it a density of 1.
-   self.fixture:setRestitution(0.9) --let the ball bounce
-   self.body:setLinearDamping(2);
+   self.fixture = love.physics.newFixture(self.body, self.shape, 1)
+   self.fixture:setRestitution(0.9)
+   self.body:setLinearDamping(2)
    
    if(image) then self.image=image end
 end
@@ -21,12 +21,12 @@ function ball:kick(angle, magnitude)
 end
 
 function ball:draw()
-   love.graphics.setColor(255,255,255,255)
-   if(objects.ball.image) then
-      love.graphics.draw(objects.ball.image, objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.body:getAngle(),  1, 1, objects.ball.image:getWidth()/2, objects.ball.image:getHeight()/2)
+   if(self.image) then
+      love.graphics.setColor(255,255,255,255)
+      love.graphics.draw(self.image, self.body:getX(), self.body:getY(), self.body:getAngle(),  1, 1, self.image:getWidth()/2, self.image:getHeight()/2)
    else
       love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
-      love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
+      love.graphics.circle("fill", self.body:getX(), self.body:getY(), self.shape:getRadius())
    end
    
 end
