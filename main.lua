94e9d6c5c857c2ffd190ff80649ca5dfa862b8d9
Coda <codabrink@gmail.com>
2013-01-02 19:11:26 -0500

    move ball movements to love.keypressed

--- main.lua
+++ main.lua
@@ -25,6 +25,14 @@ end
 function love.keypressed(key)
    if key == "q" then
       love.event.push("quit")
+   elseif key == "right" then
+      objects.ball:kick(90, 75)   
+   elseif key == "left" then
+      objects.ball:kick(270, 75)
+   elseif key == "up" then
+      objects.ball:kick(0, 75)
+   elseif key == "down" then
+      objects.ball:kick(180, 75)
    end
 end
 
@@ -40,16 +48,6 @@ function love.update(dt)
 	      player:update()
 	      player:govern()
 	   end
-	end
-
-	if love.keyboard.isDown("right") then
-		objects.ball:kick(90, 75)
-	elseif love.keyboard.isDown("left") then
-		objects.ball:kick(270, 75)
-	elseif love.keyboard.isDown("down") then
-		objects.ball:kick(180, 75)
-	elseif love.keyboard.isDown("up") then
-		objects.ball:kick(0, 75)
 	end
 end
 
