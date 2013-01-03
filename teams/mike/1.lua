4d08fc01416b792d334a82be88823940e540cdc3
Coda <codabrink@gmail.com>
2013-01-02 20:30:18 -0500

    proprietary file for mike

--- teams/mike/1.lua
+++ teams/mike/1.lua
@@ -1,10 +1,10 @@
 require "lib/class"
 require "teams/player"
-require "teams/coda/proprietary"
-class "mike1" : extends(player)
+require "teams/mike/proprietary"
+class "mike1" : extends(proprietary)
 
 function mike1:init(team)
-   self:createPlayer(team, 100, 100)
+   self:createPlayer(team, 110, 100)
 end
 
 function mike1:update(dt)
